cd7d1517a708011f6d2a5a66461c6bfa1b1d9a4c
jhony maurad <jhonymaurad@gmail.com>
2019-06-18 16:56:39 -0400

    Add CarouselPet and about components with css files

---
+++ src/components/CarouselPet.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Carousel, Button } from 'antd';
+import 'antd/dist/antd.css';
+import '../css/CarouselPet.css';
+
+function onChange(a, b, c) {
+  console.log(a, b, c);
+}
+const CarouselPet =() =>{
+return(
+  <div>
+    <Carousel className='container' afterChange={onChange} >
+      <div className="slide slide1">
+        <h1 className='main-text'>Where Pets Find Their People</h1><br></br> <br></br> <br></br> <br></br>
+        <h3 className='sub-text'>Thousands of adoptable pets are looking for people. People like you.</h3>
+      </div>
+      <div className='slide slide2'>
+        <h1 className='main-text'>Finding the Right Pet</h1> <br></br> <br></br> <br></br> <br></br>
+        <h3 className='sub-text'>Get tips on figuring out who you should adopt.</h3>
+      </div>
+      <div className='slide slide3'>
+        <h1 className='main-text'>From one good home to another</h1> <br></br> <br></br> <br></br> <br></br>
+        <h3 className='sub-text'>Rehoming a dog or a cat keeps them out of shelters.</h3>
+      </div>
+      <div className='slide slide4'> 
+        <h1 className='main-text'>Pet adoption with less stress. </h1> <br></br> <br></br> <br></br> <br></br>
+        <h3 className='sub-text'>It’s easy. Search by pet type, gender, location: dig deep into traits, breed, history and more.</h3>
+      </div>
+    </Carousel>
+
+    <div className= 'button-container'>
+      <Button ghost>Adopt a Pet</Button>
+      <Button ghost>Rehome a Pet</Button>
+    </div>
+
+</div>
+);
+}
+
+export default CarouselPet;
---
+++ src/components/about.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../css/about.css';
+import Logo from  '../assets/logo.png';
+import about1 from '../assets/about1.jpg';
+import about2 from '../assets/about2.jpg';
+
+
+export default () => (
+  <div className='welcome-container'>
+    <img src={Logo} alt='logo'></img>
+    <h1 className='title'>About us</h1>
+    <h2 className='second-line'>We're Pet Lovers Trying to Make a Difference</h2>
+    <img src={about1} alt='logo'></img>
+    <p className='welcome-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+      dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+      sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <img src={about2} alt='logo'></img>
+  </div>
+);
