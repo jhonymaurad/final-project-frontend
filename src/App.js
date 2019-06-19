5c28ab62ed4bb96f297df33886e707b17ac4d67e
Edward Biswas <edbiswas@gmail.com>
2019-06-19 10:22:06 -0400

    Fixed layout issues

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React, { Fragment } from "react";
 import {
   BrowserRouter as Router,
@@ -6,17 +5,14 @@ import {
   Switch,
   Redirect
 } from "react-router-dom";
-import { Container } from "reactstrap";
-import './App.css';
+import "./App.css";
 
-import CarouselPet from './components/CarouselPet';
-import About from './components/about';
+import CarouselPet from "./components/CarouselPet";
+import About from "./components/about";
 import NavigationBar from "./components/NavigationBar";
-import contact from './pages/contact';
-import home from './pages/home';
+import contact from "./pages/contact";
 import { Account } from "./pages/Account";
 import { UserDashboard } from "./pages/UserDashboard";
-
 
 function App() {
   return (
@@ -24,15 +20,13 @@ function App() {
       <Router>
         <NavigationBar />
         <div className="App">
-          <Container>
-            <Switch>
-              <Route exact path="/" component={CarouselPet} />
-              <Route exact path="/about" component={About} />
-              <Route path="/contact" component={contact} />
-              <Route exact path="/account" component={Account} />
-              <Route exact path="/account/dashboard" component={UserDashboard} />
-            </Switch>
-          </Container>
+          <Switch>
+            <Route exact path="/" component={CarouselPet} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={contact} />
+            <Route exact path="/account" component={Account} />
+            <Route exact path="/account/dashboard" component={UserDashboard} />
+          </Switch>
         </div>
       </Router>
     </Fragment>
