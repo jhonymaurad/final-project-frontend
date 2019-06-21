09226d4d0a38f7e9dd810f26ebf53cc013486de0
jhony maurad <jhonymaurad@gmail.com>
2019-06-21 12:23:19 -0400

    Updates

---
+++ precache-manifest.b587cad9417f7e1329a48cd31e79e462.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f77f2b37125e42cc0f9c32c6e3bd95a",
+    "url": "/final-project-frontend/index.html"
+  },
+  {
+    "revision": "f784edc018ff57e0e296",
+    "url": "/final-project-frontend/static/css/2.d31301e0.chunk.css"
+  },
+  {
+    "revision": "1690aeb3e643c2bbf668",
+    "url": "/final-project-frontend/static/css/main.49e5caab.chunk.css"
+  },
+  {
+    "revision": "f784edc018ff57e0e296",
+    "url": "/final-project-frontend/static/js/2.366cf00f.chunk.js"
+  },
+  {
+    "revision": "1690aeb3e643c2bbf668",
+    "url": "/final-project-frontend/static/js/main.af367504.chunk.js"
+  },
+  {
+    "revision": "9c44a2cc5dd4637f3c50",
+    "url": "/final-project-frontend/static/js/runtime~main.c360060d.js"
+  },
+  {
+    "revision": "d974fb5c27602d55d16dc4ff9049843b",
+    "url": "/final-project-frontend/static/media/about1.d974fb5c.jpg"
+  },
+  {
+    "revision": "9053a4f1a5576190470036fb5043461c",
+    "url": "/final-project-frontend/static/media/about2.9053a4f1.jpg"
+  },
+  {
+    "revision": "4f325f5595a05428bfd8422c9ab9ab4c",
+    "url": "/final-project-frontend/static/media/dog-avatar.4f325f55.png"
+  },
+  {
+    "revision": "3687f488602fb164d6de5154e34df1f8",
+    "url": "/final-project-frontend/static/media/nav-logo.3687f488.png"
+  },
+  {
+    "revision": "dbc89779fb4fc6b0dcd08612cc347215",
+    "url": "/final-project-frontend/static/media/slide1.dbc89779.jpg"
+  },
+  {
+    "revision": "1342c85ff3f6d411c4d576fab26b1387",
+    "url": "/final-project-frontend/static/media/slide2.1342c85f.jpg"
+  },
+  {
+    "revision": "79526cbade77ff1c1c292b76ca1b4159",
+    "url": "/final-project-frontend/static/media/slide3.79526cba.jpg"
+  },
+  {
+    "revision": "2eb16a646a9b207b34c3d6cc71b899af",
+    "url": "/final-project-frontend/static/media/slide4.2eb16a64.jpg"
+  }
+]);
